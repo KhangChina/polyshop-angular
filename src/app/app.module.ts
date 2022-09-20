4d6b2705701cf042bc0bdf95c07655ba28674640
Khang Nguyễn <nguyenkhang1400@gmail.com>
2022-09-20 11:07:31 +0700

    creater admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductModifyComponent } from './components/product-modify/product-modify.component';
+import { AdminRoutes } from './routers/admin.routing';
 
 const router: Routes =[
   {path:'productlist',component:ProductListComponent},
   {path:'productdetail/:id',component:ProductModifyComponent},
+  {path:'',pathMatch:'full',redirectTo:'productlist'},
+  {path:'**',component:NotFoundComponent}
 ]
 
 @NgModule({
@@ -20,11 +24,13 @@ const router: Routes =[
     ProductListComponent,
     ProductDetailComponent,
     ProductModifyComponent,
-    MenuComponent
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,  
     FormsModule,
+    AdminRoutes,
     RouterModule.forRoot(router)
   ],
   providers: [],
